c7a0b42d59b26ed73807fefc19ca4fddee909efd
fbslo <admin@fbslo.net>
2021-11-04 22:20:19 +0100

    update migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -17,8 +17,8 @@ const MockERC20 = artifacts.require("MockToken");
 
 let wmatic = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270'
 let govAddress = '0xA1982835170d0C2ba789370918F19122D63943A2'
-let lionsDenAddress = '0xC11E1b8225a0eeEf5AA1fD2B88170D470dB82386'
-let cub = '0x173848c59b3eb6f72c213ad64c0312f0acae21eb'
+let lionsDenAddress = MasterChef.address
+let cub = Token.address
 let sushiEarned = '0x0b3F868E0BE5597D5DB7fEB59E1CADBb0fdDa50a'
 let sushiFarm = '0x0769fd68dFb93167989C6f7254cd0D766Fb2841F'
 let sushiRouter = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506'
@@ -48,42 +48,40 @@ let wmatic_quick_addresses = [
   quickFarm, quickRouter, rewardsAddress, deadAddress
 ]
 
-const testAdminAddress = '0x3eCBE3F53D3DeAebEBb4336Aa269afdff23da3FA'
-
 module.exports = async function (deployer, network, accounts) {
   // deploy contracts
-  await deployer.deploy(Token);
-  await deployer.deploy(MasterChef, adminAddress, 0, Token.address)
-  await deployer.deploy(Staker, Token.address, adminAddress, MasterChef.address)
-
-  //mint 1.3M tokens
-  const tokenInstance = await Token.deployed();
-  await tokenInstance.mint(adminAddress, '1300000000000000000000000').sendTransaction()
-
-  // transfer token ownership
-  await tokenInstance.transferOwnership(MasterChef.address).sendTransaction()
+  // await deployer.deploy(Token);
+  // await deployer.deploy(MasterChef, govAddress, 21050000, Token.address)
+  // await deployer.deploy(Staker, Token.address, govAddress, MasterChef.address)
+  //
+  // //mint 1.3M tokens
+  // const tokenInstance = await Token.deployed();
+  // await tokenInstance.mint(govAddress, '1300000000000000000000000', {from:accounts[0]})
+  //
+  // // transfer token ownership
+  // await tokenInstance.transferOwnership(MasterChef.address, {from:accounts[0]})
 
   // WETH-WBTC vault
-  await deployer.deploy(
-    SushiVault, weth_wbtc_addresses, 3, false, true, sushiEarnedToCub,
-    [sushiEarned, wmatic, weth_wbtc_addresses[5]], [sushiEarned, wmatic, weth_wbtc_addresses[6]],
-    [weth_wbtc_addresses[5], wmatic, sushiEarned], [weth_wbtc_addresses[6], wmatic, sushiEarned],
-    1000, 0, 9990, 10000, govAddress
-  )
-
-  // WETH-DAI vault
+  // await deployer.deploy(
+  //   SushiVault, weth_wbtc_addresses, 3, false, true, sushiEarnedToCub,
+  //   [sushiEarned, wmatic, weth_wbtc_addresses[5]], [sushiEarned, wmatic, weth_wbtc_addresses[6]],
+  //   [weth_wbtc_addresses[5], wmatic, sushiEarned], [weth_wbtc_addresses[6], wmatic, sushiEarned],
+  //   1000, 0, 9990, 10000, govAddress
+  // )
+  //
+  // // WETH-DAI vault
   await deployer.deploy(
     SushiVault, weth_dai_addresses, 5, false, true, sushiEarnedToCub,
     [sushiEarned, wmatic, weth_dai_addresses[5]], [sushiEarned, wmatic, weth_dai_addresses[6]],
     [weth_dai_addresses[5], wmatic, sushiEarned], [weth_dai_addresses[6], wmatic, sushiEarned],
     1000, 0, 9990, 10000, govAddress
   )
-
-  // WMATIC-QUICK vault
-  await deployer.deploy(
-    QuickVault, wmatic_quick_addresses, false, true, quickEarnedToCub,
-    [quickEarned, wmatic, wmatic_quick_addresses[5]], [quickEarned, wmatic, wmatic_quick_addresses[6]],
-    [wmatic_quick_addresses[5], wmatic, quickEarned], [wmatic_quick_addresses[6], wmatic, quickEarned],
-    1000, 0, 9990, 10000, govAddress
-  )
+  //
+  // // WMATIC-QUICK vault
+  // await deployer.deploy(
+  //   QuickVault, wmatic_quick_addresses, false, true, quickEarnedToCub,
+  //   [quickEarned, wmatic, wmatic_quick_addresses[5]], [quickEarned, wmatic, wmatic_quick_addresses[6]],
+  //   [wmatic_quick_addresses[5], wmatic, quickEarned], [wmatic_quick_addresses[6], wmatic, quickEarned],
+  //   1000, 0, 9990, 10000, govAddress
+  // )
 };
