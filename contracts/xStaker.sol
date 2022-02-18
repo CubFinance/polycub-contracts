38ef45c6f48b23f6f59fb12fe66b90530e20412a
fbslo <admin@fbslo.net>
2022-02-18 16:05:00 +0100

    Resolve: SCF-01

--- contracts/xStaker.sol
+++ contracts/xStaker.sol
@@ -705,6 +705,10 @@ contract xStaker is ERC20("xPolyCub", "xPOLYCUB"){
     bool public rewardsEnabled = false;
 
     constructor(IERC20 _token, address _admin, address _mainStaking) public {
+        require(_token != address(0), "!address(0)");
+        require(_admin != address(0), "!address(0)");
+        require(_mainStaking != address(0), "!address(0)");
+
         token = _token;
         admin = _admin;
         mainStakingContract = _mainStaking;
